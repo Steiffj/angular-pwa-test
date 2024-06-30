c085646b0f48f445914a7b982a94f71c4b022dfb
Steiffj <18404081+Steiffj@users.noreply.github.com>
2024-06-29 23:31:17 -0400

    Run data sync on app startup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataSyncService } from './store/data-sync.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  readonly dataSyncSvc = inject(DataSyncService);
   title = 'pwa';
+
+  ngOnInit() {
+    console.log('OnInit lifecycle hook - loading pokemon by type');
+    this.loadPokemon();
+  }
+
+  async loadPokemon() {
+    await this.dataSyncSvc.initWebWorker();
+    this.dataSyncSvc.loadPokemonByType();
+  }
 }
