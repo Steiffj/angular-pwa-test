53b2cf56232b8217f7ce3793e475a81253bba848
Steiffj <18404081+Steiffj@users.noreply.github.com>
2024-07-20 14:00:27 -0400

    Handle updated un/register messages.

--- src/app/workers/shared-worker-handler.ts
+++ src/app/workers/shared-worker-handler.ts
@@ -10,43 +10,19 @@ import {
 } from '../shared-worker/messages';
 import { Pokemon } from '../store/pokemon';
 import { IDB, TypesIDB } from '../store/worker-generics';
-import {
-  MsgPort,
-  SharedWorkerOnconnect,
-} from '../worker-types/typed-shared-worker';
+import { SharedWorkerOnconnect } from '../worker-types/typed-shared-worker';
+import { Multicaster } from './multicaster';
+import { ViewName } from 'views/view-name';
 
 export class SharedWorkerHandler {
   // Track which data each rx is interested in.
-  readonly ports = new Map<SharedWorkerMsgName, MsgPort<SharedWorkerMsg>[]>();
+  readonly multicaster = new Multicaster<
+    SharedWorkerMsgName,
+    SharedWorkerMsg
+  >();
+  readonly connectedViews = new Set<ViewName>();
+
   #graph: Graph = new Graph();
-
-  private setPort(
-    port: MsgPort<SharedWorkerMsg>,
-    messages: SharedWorkerMsgName[]
-  ) {
-    const registered: SharedWorkerMsgName[] = [];
-    for (const msg of messages) {
-      const ports = this.ports.get(msg) ?? [];
-      if (!ports.includes(port)) {
-        this.ports.set(msg, [port, ...ports]);
-        registered.push(msg);
-      }
-    }
-    return registered;
-  }
-
-  private clearPort(port: MsgPort<SharedWorkerMsg>) {
-    for (const activePorts of this.ports.values()) {
-      const index = activePorts.indexOf(port);
-      if (index > -1) {
-        activePorts.splice(index, 1);
-      }
-    }
-  }
-
-  private getPorts(msg: SharedWorkerMsgName) {
-    return this.ports.get(msg) ?? [];
-  }
 
   constructor(readonly openDB: typeof idb.openDB<TypesIDB>) {}
 
@@ -57,7 +33,11 @@ export class SharedWorkerHandler {
       const name = data.name;
       switch (name) {
         case 'register': {
-          const registered = this.setPort(port, data.payload);
+          this.connectedViews.add(data.payload.view);
+          const registered = this.multicaster.setPort(
+            port,
+            data.payload.messages
+          );
           port.postMessage({
             name,
             response: {
@@ -68,12 +48,13 @@ export class SharedWorkerHandler {
           break;
         }
         case 'unregister': {
-          this.clearPort(port);
+          this.connectedViews.add(data.payload);
+          this.multicaster.clearPort(port);
           break;
         }
         case 'get-list-of-type': {
           const res = await this.getListOfType(data.payload);
-          for (const rx of this.getPorts(name)) {
+          for (const rx of this.multicaster.getPorts(name)) {
             rx.postMessage({
               name,
               response: {
@@ -85,7 +66,7 @@ export class SharedWorkerHandler {
           break;
         }
         case 'misc': {
-          for (const rx of this.getPorts(name)) {
+          for (const rx of this.multicaster.getPorts(name)) {
             rx.postMessage({
               name,
               response: {
@@ -99,7 +80,7 @@ export class SharedWorkerHandler {
         case 'get-graph': {
           this.#graph = await this.getPokemonTypeGraph(data.payload);
           const serialized = this.#graph.export();
-          for (const rx of this.getPorts(name)) {
+          for (const rx of this.multicaster.getPorts(name)) {
             rx.postMessage({
               name,
               response: {
@@ -135,27 +116,27 @@ export class SharedWorkerHandler {
 
   async getPokemonTypeGraph(types: PokemonType[]) {
     console.log('Creating graph from pokemon types');
-    // const idb: IDB<TypesIDB> = await this.openDB('poke-types', 1);
-    // const tx = idb.transaction(types, 'readonly');
+    const idb: IDB<TypesIDB> = await this.openDB('poke-types', 1);
+    const tx = idb.transaction(types, 'readonly');
     const graph = new Graph();
-    // for (const type of types) {
-    //   graph.addNode(type, {
-    //     label: type,
-    //     size: 10,
-    //   });
-    //   const store = tx.objectStore(type);
-    //   for await (const cursor of store.iterate()) {
-    //     const node = cursor.value;
-    //     if (!graph.hasNode(node.id)) {
-    //       graph.addNode(node.id, { ...node, label: node.name, size: 2 });
-    //     }
-    //     graph.addDirectedEdge(node.id, type, {
-    //       label: 'is type',
-    //     });
-    //   }
-    // }
+    for (const type of types) {
+      graph.addNode(type, {
+        label: type,
+        size: 10,
+      });
+      const store = tx.objectStore(type);
+      for await (const cursor of store.iterate()) {
+        const node = cursor.value;
+        if (!graph.hasNode(node.id)) {
+          graph.addNode(node.id, { ...node, label: node.name, size: 2 });
+        }
+        graph.addDirectedEdge(node.id, type, {
+          label: 'is type',
+        });
+      }
+    }
 
-    // await tx.done;
+    await tx.done;
     circular.assign(graph);
     return graph;
   }
