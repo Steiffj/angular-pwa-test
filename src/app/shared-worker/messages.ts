2fc6f071d403ed29732e792f50065cc65ba59103
Steiffj <18404081+Steiffj@users.noreply.github.com>
2024-07-21 13:13:20 -0400

    Rename shared worker message name array.

--- src/app/shared-worker/messages.ts
+++ src/app/shared-worker/messages.ts
@@ -4,37 +4,37 @@ import { SerializedGraph } from 'graphology-types';
 import { Pokemon } from 'store/pokemon';
 import { ViewName } from 'views/view-name';
 
-const SHARED_WORKER_MESSAGE_TYPES = [
+const SHARED_WORKER_MESSAGE_NAMES = [
   'register',
   'unregister',
   'misc',
   'get-list-of-type',
   'get-graph',
 ] as const;
-export type SharedWorkerMsgName = (typeof SHARED_WORKER_MESSAGE_TYPES)[number];
+export type SharedWorkerMsgName = (typeof SHARED_WORKER_MESSAGE_NAMES)[number];
 
 export type RegisterMsg = MsgStruct<
-  (typeof SHARED_WORKER_MESSAGE_TYPES)[0],
+  (typeof SHARED_WORKER_MESSAGE_NAMES)[0],
   { view: ViewName; messages: SharedWorkerMsgName[] },
   SharedWorkerMsgName[]
 >;
 export type UnregisterMsg = MsgStruct<
-  (typeof SHARED_WORKER_MESSAGE_TYPES)[1],
+  (typeof SHARED_WORKER_MESSAGE_NAMES)[1],
   ViewName,
   boolean
 >;
 export type MiscMsg = MsgStruct<
-  (typeof SHARED_WORKER_MESSAGE_TYPES)[2],
+  (typeof SHARED_WORKER_MESSAGE_NAMES)[2],
   string,
   string
 >;
 export type GetListOfTypeMsg = MsgStruct<
-  (typeof SHARED_WORKER_MESSAGE_TYPES)[3],
+  (typeof SHARED_WORKER_MESSAGE_NAMES)[3],
   PokemonType,
   Pokemon[]
 >;
 export type GetGraphMsg = MsgStruct<
-  (typeof SHARED_WORKER_MESSAGE_TYPES)[4],
+  (typeof SHARED_WORKER_MESSAGE_NAMES)[4],
   PokemonType[],
   SerializedGraph
 >;
